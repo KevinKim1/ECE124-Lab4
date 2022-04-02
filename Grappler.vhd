e20ca32728b5ebe067034be6cf4be7b0059d891e
Jonah Walker <jonahlwalker@gmail.com>
2022-04-01 21:09:25 -0400

    grappler comments

--- Grappler.vhd
+++ Grappler.vhd
@@ -30,10 +30,10 @@ BEGIN
  
 Register_Section: PROCESS (clk_input, reset, next_state)  -- this process synchronizes the activity to a clock
 BEGIN
-	IF (reset = '1') THEN
-		current_state <= s_open;
+	IF (reset = '1') THEN	-- reset signal
+		current_state <= s_open;		-- set back to initial state
 	ELSIF(rising_edge(clk_input)) THEN
-		current_state <= next_state;
+		current_state <= next_state;	-- on rising edge, transition to next state
 	END IF;
 END PROCESS;	
 
@@ -45,27 +45,27 @@ Transition_Section: PROCESS (grappler, grappler_en, current_state)
 
 BEGIN
     CASE current_state IS
-         WHEN s_open =>		
-				IF(grappler='1' AND grappler_en='1') THEN
-					next_state <= s_close_btn_active;
+         WHEN s_open =>		-- initial state - when the grappler is open
+				IF(grappler='1' AND grappler_en='1') THEN 	-- grappler is to be toggled and extender is fully extended
+					next_state <= s_close_btn_active;			-- close state and button has just been pressed
 				ELSE
-					next_state <= s_open;
+					next_state <= s_open;							-- stay in same state
 				END IF;
 
-         WHEN s_close_btn_active =>		
-				IF(grappler='0' AND grappler_en='1') THEN
-					next_state <= s_close_btn_inactive;
-				ELSIF(grappler='1' AND grappler_en='1') THEN
-					next_state <= s_close_btn_active;
+         WHEN s_close_btn_active =>								-- grappler is closed and button was just pressed
+				IF(grappler='0' AND grappler_en='1') THEN		-- grappler not to be toggled but grappler is still active
+					next_state <= s_close_btn_inactive;			-- inactive button state
+				ELSIF(grappler='1' AND grappler_en='1') THEN		-- grappler is still receiving input
+					next_state <= s_close_btn_active;				-- stay in same state
 				ELSE
-					next_state <= s_open;
+					next_state <= s_open;								-- open grappler when grappler is no longer enabled
 				END IF;
 				
 			WHEN s_close_btn_inactive =>		
-				IF(grappler='0' AND grappler_en='0') THEN
+				IF(grappler='0' AND grappler_en='0') THEN			-- grappler to be toggled open
 					next_state <= s_open;
 				ELSE
-					next_state <= s_close_btn_inactive;
+					next_state <= s_close_btn_inactive;				-- otherwise stay closed
 				END IF;
 
  		END CASE;
@@ -78,14 +78,14 @@ Decoder_Section: PROCESS (current_state)
 
 BEGIN
     CASE current_state IS
-         WHEN s_open =>		
-				grappler_on <= '0';
+         WHEN s_open =>	
+				grappler_on <= '0';				-- grappler is open (0)
 			
          WHEN s_close_btn_active =>		
-				grappler_on <= '1';
+				grappler_on <= '1';				-- grappler is closed (1)
 			
 			WHEN s_close_btn_inactive =>		
-				grappler_on <= '1';
+				grappler_on <= '1';				-- grappler is closed (1)
 
 	  END CASE;
  END PROCESS;
